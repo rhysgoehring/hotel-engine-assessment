35a2688e31188d61ed795b18d5d7d4d6cb9ad83a
rhysgoehring <rhysgoehring@gmail.com>
2019-11-13 10:35:41 -0700

    removed console statement

--- src/App.js
+++ src/App.js
@@ -25,7 +25,6 @@ function App() {
   const fetchRepo = async searchTerm => {
     try {
       const { data } = await axios.post('/api/search', { searchTerm });
-      console.log('response', data);
       setRepos(data);
     } catch (error) {
       console.log('error fetching repo', error);
